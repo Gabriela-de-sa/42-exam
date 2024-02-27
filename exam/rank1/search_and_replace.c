3c79bb3c3d4fc7cad02b5ee3cf47e9ff0f086115
Gabriela de Sá <84455552+Gabriela-de-sa@users.noreply.github.com>
2024-02-27 19:27:00 -0300

    Create search_and_replace.c

---
+++ exam/rank1/search_and_replace.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search_and_replace.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabriela <gabriela@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 19:13:28 by gabriela          #+#    #+#             */
+/*   Updated: 2024/02/27 19:25:55 by gabriela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name  : search_and_replace
+Expected files   : search_and_replace.c
+Allowed functions: write, exit
+--------------------------------------------------------------------------------
+
+Write a program called search_and_replace that takes 3 arguments, the first
+arguments is a string in which to replace a letter (2nd argument) by
+another one (3rd argument).
+
+If the number of arguments is not 3, just display a newline.
+
+If the second argument is not contained in the first one (the string)
+then the program simply rewrites the string followed by a newline.
+
+Examples:
+$>./search_and_replace "Papache est un sabre" "a" "o"
+Popoche est un sobre
+$>./search_and_replace "zaz" "art" "zul" | cat -e
+$
+$>./search_and_replace "zaz" "r" "u" | cat -e
+zaz$
+$>./search_and_replace "jacob" "a" "b" "c" "e" | cat -e
+$
+$>./search_and_replace "ZoZ eT Dovid oiME le METol." "o" "a" | cat -e
+ZaZ eT David aiME le METal.$
+$>./search_and_replace "wNcOre Un ExEmPle Pas Facilw a Ecrirw " "w" "e" | cat -e
+eNcOre Un ExEmPle Pas Facile a Ecrire $
+
+-- o primeiro argumento é uma string
+-- o 2 e 3 somente pode ter uma letra
+-- se a letra da string for igual a letra do segundo argumento, a letra da string em questao é
+	alterada pela letra do terceiro argumento
+-- somente pode ter 4 argumentos contando com o executavel
+*/
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc == 4 && !argv[2][1] && !argv[3][1])
+	{
+		while (argv[1][i])
+		{
+			if (argv[1][i] == argv[2][0])
+				argv[1][i] = argv[3][0];
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+}
